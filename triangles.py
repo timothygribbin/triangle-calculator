4c30f1f8dcd5a74d6bfa0d934384927f9c743c4a
timothygribbin <98505150+timothygribbin@users.noreply.github.com>
2023-05-01 15:08:45 -0400

    Update triangles.py

--- triangles.py
+++ triangles.py
@@ -20,16 +20,14 @@ def A():
         side_a = float(input("Side a: "))
         side_b = float(input("Side b: "))
         angle_C = float(input("Angle C in degrees: "))
-        side_c = math.sqrt(side_a ** 2 + side_b ** 2 - (2 *side_a) * (side_b) * math.cos(math.radians(angle_C)))
+        side_c = law_of_cos_side(side_a,side_b,angle_C)
         smallest_side = min([side_a, side_b])
         if smallest_side == side_b:
-            sin_b = (side_b * math.degrees(math.sin(math.radians(angle_C)))/ side_c)
-            angle_B = math.degrees(math.asin(math.radians(sin_b)))
-            angle_A = 180 - (angle_C + angle_B)
+            angle_B = law_of_sin_angle(side_b, angle_C, side_c)
+            angle_A = third_angle(angle_C, angle_B)
         if smallest_side == side_a: 
-            sin_a = (side_a * math.degrees(math.sin(math.radians(angle_C)))/ side_c)
-            angle_A = math.degrees(math.asin(math.radians(sin_a)))
-            angle_B = 180 - (angle_C + angle_A)
+            angle_A = law_of_sin_angle(side_a, angle_C, side_c)
+            angle_B = third_angle(angle_C, angle_A)
         area = calculate_area(side_a,side_b,side_c)
         print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area) 
 
@@ -37,26 +35,30 @@ def B():
     angle_A = float(input(("Angle A: ")))
     angle_B = float(input(("Angle B: ")))
     side_c = float(input(("Side c: ")))
-    angle_C = 180 - (angle_A + angle_B)
-    side_a = ((side_c * math.degrees(math.sin(math.radians(angle_A))))/ math.degrees(math.sin(math.radians(angle_C))))
-    side_b = ((side_c * math.sin(math.radians(angle_B)))/math.sin(math.radians(angle_C)))
+    angle_C = third_angle(angle_A, angle_B)
+    side_a = law_of_sin_side(side_c, angle_A,angle_C)
+    side_b = law_of_sin_side(side_c,angle_B,angle_C)
     area = calculate_area(side_a,side_b,side_c)
-    print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area)    
+    print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area)  
 
 def C():
      side_a = float(input(("Side a: ")))
      side_b = float(input(("Side b: ")))
      side_c = float(input(("Side c: ")))
-     if(side_a + side_c <= side_b):
-          print("Not a valid Triangle inequality: a + c <= b")
-          return
-     cos_c  = (side_a ** 2 + side_b ** 2 - side_c ** 2) / (2* (side_a ) * (side_b)) 
-     angle_C = math.degrees(math.acos((cos_c)))
-     sin_a = (side_a  * math.degrees(math.sin(math.radians(angle_C)))) / side_c
-     angle_A = math.degrees(math.asin((math.radians(sin_a))))
-     angle_B = 180-(angle_A + angle_C)
+     if(side_a + side_c <= side_b) or (side_a + side_b <= side_c) or (side_b + side_c <= side_a):
+        print("Not a valid Triangle inequality: a + c <= b")
+        return
+     elif(side_a + side_b <= side_c):   
+        print("Not a valid Triangle inequality: a + b <= c")
+        return
+     elif(side_b + side_c <= side_a):
+        print("Not a valid Triangle inequality: b + c <= a")
+        return
+     angle_C = law_of_cos_angle(side_a, side_b,side_c)
+     angle_A = law_of_cos_angle(side_b,side_c,side_a)
+     angle_B = third_angle(angle_A, angle_C)
      area = calculate_area(side_a,side_b,side_c)
-     print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area)   
+     print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area) 
 
 def print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area):
     print("\nResults:\n")
@@ -71,4 +73,21 @@ def print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area):
 def calculate_area(side_a,side_b,side_c):
     s = (side_a + side_b + side_c) / 2
     area = math.sqrt(s*(s-side_a)*(s-side_b)*(s-side_c))
+    return area
+
+def law_of_sin_angle(x,y,z):
+     return math.degrees(math.asin(math.radians((x * math.degrees(math.sin(math.radians(y)))/ z))))
+
+def law_of_sin_side(x,y,z):
+     return ((x * math.degrees(math.sin(math.radians(y))))/ math.degrees(math.sin(math.radians(z))))
+
+def law_of_cos_side(x,y,z):
+    return math.sqrt(x ** 2 + y ** 2 - (2 *x) * (y) * math.cos(math.radians(z)))
+
+def law_of_cos_angle(x,y,z):
+     return math.degrees(math.acos((x ** 2 + y ** 2 - z ** 2)/ (2 * x * y)))
+     
+def third_angle(a,b):
+     return 180 - (a + b)
+
 main()
