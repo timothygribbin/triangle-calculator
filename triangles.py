8c11ccf3ef2daa43da8d45984756ce9a5a5038bf
timothygribbin <98505150+timothygribbin@users.noreply.github.com>
2023-05-06 19:56:58 -0400

    Update triangles.py

--- triangles.py
+++ triangles.py
@@ -1,12 +1,14 @@
 import math
+from sys import exit
+
 def main():
     ans = prompt()
     if ans == "A":
-        A()
+        sas_triangle_computation()
     elif ans == "B":
-         B()
+         asa_triangle_computation()
     elif ans =="C":
-         C()
+         sss_triangle_computation()
     else:
          print("Answer not found, please run the program again and enter 'A', 'B', or 'C'.")
 
@@ -16,49 +18,41 @@ def prompt():
         x = input("What information will you be providing me with today? \n A) 2 sides and the angle between those 2 sides \n B) 2 angles and one side in between those angles \n C) 3 sides and no angles \n Enter your letter choice below: \n").capitalize().strip()
     return x
 
-def A():
-        side_a = float(input("Side a: "))
-        side_b = float(input("Side b: "))
-        angle_C = float(input("Angle C in degrees: "))
-        side_c = law_of_cos_side(side_a,side_b,angle_C)
-        smallest_side = min([side_a, side_b])
-        if smallest_side == side_b:
-            angle_B = law_of_sin_angle(side_b, angle_C, side_c)
-            angle_A = third_angle(angle_C, angle_B)
-        if smallest_side == side_a: 
-            angle_A = law_of_sin_angle(side_a, angle_C, side_c)
-            angle_B = third_angle(angle_C, angle_A)
-        area = calculate_area(side_a,side_b,side_c)
-        print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area) 
+def sas_triangle_computation():
+    side_a = side_prompt_and_validation_a()
+    side_b = side_prompt_and_validation_b()
+    angle_C = angle_prompt_and_validation_c()
+    side_c = law_of_cos_side(side_a,side_b,angle_C)
+    smallest_side = min([side_a, side_b])
+    if smallest_side == side_b:
+        angle_B = law_of_sin_angle(side_b, angle_C, side_c)
+        angle_A = third_angle(angle_C, angle_B)
+    if smallest_side == side_a: 
+        angle_A = law_of_sin_angle(side_a, angle_C, side_c)
+        angle_B = third_angle(angle_C, angle_A)
+    area = calculate_area(side_a,side_b,side_c)
+    print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area) 
 
-def B(): 
-    angle_A = float(input(("Angle A: ")))
-    angle_B = float(input(("Angle B: ")))
-    side_c = float(input(("Side c: ")))
+def asa_triangle_computation(): 
+    angle_A = angle_prompt_and_validation_a()
+    angle_B = angle_prompt_and_validation_b()
+    side_c = side_prompt_and_validation_c()
     angle_C = third_angle(angle_A, angle_B)
     side_a = law_of_sin_side(side_c, angle_A,angle_C)
     side_b = law_of_sin_side(side_c,angle_B,angle_C)
     area = calculate_area(side_a,side_b,side_c)
     print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area)  
 
-def C():
-     side_a = float(input(("Side a: ")))
-     side_b = float(input(("Side b: ")))
-     side_c = float(input(("Side c: ")))
-     if(side_a + side_c <= side_b) or (side_a + side_b <= side_c) or (side_b + side_c <= side_a):
-        print("Not a valid Triangle inequality: a + c <= b")
-        return
-     elif(side_a + side_b <= side_c):   
-        print("Not a valid Triangle inequality: a + b <= c")
-        return
-     elif(side_b + side_c <= side_a):
-        print("Not a valid Triangle inequality: b + c <= a")
-        return
-     angle_C = law_of_cos_angle(side_a, side_b,side_c)
-     angle_A = law_of_cos_angle(side_b,side_c,side_a)
-     angle_B = third_angle(angle_A, angle_C)
-     area = calculate_area(side_a,side_b,side_c)
-     print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area) 
+def sss_triangle_computation():
+    side_a = side_prompt_and_validation_a()
+    side_b = side_prompt_and_validation_b()
+    side_c = side_prompt_and_validation_c()
+    validate_triangle(side_a,side_b,side_c)
+    angle_C = law_of_cos_angle(side_a, side_b,side_c)
+    angle_A = law_of_cos_angle(side_b,side_c,side_a)
+    angle_B = third_angle(angle_A, angle_C)
+    area = calculate_area(side_a,side_b,side_c)
+    print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area) 
 
 def print_results(side_a,side_b,side_c,angle_A,angle_B,angle_C, area):
     print("\nResults:\n")
@@ -88,6 +82,100 @@ def law_of_cos_angle(x,y,z):
      return math.degrees(math.acos((x ** 2 + y ** 2 - z ** 2)/ (2 * x * y)))
      
 def third_angle(a,b):
-     return 180 - (a + b)
+    return 180 - (a + b)
+
+def angle_prompt_and_validation_a():
+    while True:
+        a = input(("Angle A: "))
+        if not float(a.isnumeric()) or float(a) > 180:
+            print("Angles are always a numeric value and cannot exceed 180°")
+            continue
+        elif float(a) <= 0:
+            print("Angle measure must be > 0")
+        else:
+            a = float(a)
+            break
+    return a
+    
+def angle_prompt_and_validation_b():
+    while True:
+        a = input(("Angle B: "))
+        if not float(a.isnumeric()) or float(a) > 180:
+            print("Angles are always a numeric value and cannot exceed 180°")
+            continue
+        elif float(a) <= 0:
+            print("Angle measure must be > 0")
+            continue
+        else:
+            a = float(a)
+            break
+    return a
+
+def angle_prompt_and_validation_c():
+    while True:
+        a = input(("Angle C: "))
+        if not float(a.isnumeric()) or float(a) > 180:
+            print("Angles are always a numeric value and cannot exceed 180°")
+            continue
+        elif float(a) <= 0:
+            print("Angle measure must be > 0")
+            continue
+        else:
+            a = float(a)
+            break
+    return a
+
+def side_prompt_and_validation_a():
+    while True:
+        a = input(("Side a: "))
+        if not float(a.isnumeric()):
+            print("Sides are always a numeric value")
+            continue
+        elif float(a) <= 0:
+            print("Side measure must be > 0")
+            continue
+        else:
+            a = float(a)
+            break
+    return a
+
+def side_prompt_and_validation_b():
+    while True:
+        a = input(("Side b: "))
+        if not float(a.isnumeric()):
+            print("Sides are always a numeric value")
+            continue
+        elif float(a) <= 0:
+            print("Side measure must be > 0")
+            continue
+        else:
+            a = float(a)
+            break
+    return a
+
+def side_prompt_and_validation_c():
+    while True:
+        a = input(("Side c: "))
+        if not float(a.isnumeric()):
+            print("Sides are always a numeric value")
+            continue
+        elif float(a) <= 0:
+            print("Side measure must be > 0")
+            continue
+        else:
+            a = float(a)
+            break
+    return a
+
+def validate_triangle(side_a,side_b,side_c):
+    if(side_a + side_c <= side_b):
+        print("Not a valid Triangle inequality: a + c <= b")
+        exit()
+    elif(side_a + side_b <= side_c):   
+        print("Not a valid Triangle inequality: a + b <= c")
+        exit()
+    elif(side_b + side_c <= side_a):
+        print("Not a valid Triangle inequality: b + c <= a")
+        exit()
 
 main()
